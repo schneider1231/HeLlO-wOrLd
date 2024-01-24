e5fc21f3622abab104307fce763d5dad5f9ce376
Matthew Schneider <154096235+schneider1231@users.noreply.github.com>
2024-01-24 21:48:32 +0000

    Update hElLo WoRlD.py

--- hElLo WoRlD.py
+++ hElLo WoRlD.py
@@ -1,26 +1,45 @@
-string =input("Enter a sentence\n:").replace(" ","  ")#add an extra space betweeen the words so each other character will be upper and lower 
+'''
+This code asks the user to input a sentence and saves it as a string e.g. "hello world"
+Then changes each other character to uppercase and lower case to look like this "hElLo WoRLlD"
+Then changes each other word to uppercase and lower case to look like this "hello WORLD"
+'''
+
+#Get input from user then replace single spaces with double spaces
+string =input("Enter a sentence\n:").replace(" ","  ")
+
+#Create empty string which will store the modified string
 new_string =""
 
+#Print the users string un modified
 print(string.replace("  "," "))
 
-for i in range(len(string)):#loops through the lenght of the string
-    if i % 2 == 1:#goes through the index of the string for even and odd
-        new_string += string[i].upper()#uppercase the even indecies and saves it in a new string
+#loops through the lenght of the string
+for i in range(len(string)):
+    #goes through the index of the string for even and odd
+    if i % 2 == 1:
+        new_string += string[i].upper() #uppercase the even indecies and saves it in a new string
     else:
-        new_string += string[i].lower()#lowercase the odd indecies and saves it in a new string
+        new_string += string[i].lower() #lowercase the odd indecies and saves it in a new string
 
-string = string.replace("  "," ")#returns the space betweeen the words to normal
+#returns the space betweeen the words to normal
+string = string.replace("  "," ")
 
-print(f"\n{new_string.replace("  "," ")}")#print string with alternating upper/lower case characters
+#print string with alternating upper/lower case characters
+print(f"\n{new_string.replace("  "," ")}")
 
+#Create empty string which will store the modified string
 new_str =""
 
-words = string.split(" ")#turns string into a list so the contents are mutable
+#turns string into a list so the contents are mutable
+words = string.split(" ")
 
-for i in range(len(words)):#loop through the lenght of the list
-    if i % 2 == 1:#goes through the even and odds
-        new_str += words[i].upper() + " "#uppper case the even count in the list then saves it into a new string and adds a space " " at the end 
+#loop through the lenght of the list
+for i in range(len(words)):
+    #goes through the even and odds
+    if i % 2 == 1:
+        new_str += words[i].upper() + " "#uppercase the words at even indices and add a space
     else:
-        new_str += words[i].lower() + " "#lower case the odd count in the list then saves it into a new string and adds a space " " at the end 
+        new_str += words[i].lower() + " "#lowercase the words at odd indices and add a space
 
-print(f"\n{new_str}")#prints the new string made from the list with each other word being upper and lower case
+#prints the new string made from the list with each other word being upper and lower case
+print(f"\n{new_str}")
